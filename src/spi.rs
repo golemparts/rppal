9996b140bc8fc2325ea73af0ee0fb285c28e6b1d
Rene van der Meer <rene@golemparts.com>
2018-05-21 19:33:13 +0200

    Add support for SPI2 on Compute modules

--- src/spi.rs
+++ src/spi.rs
@@ -38,17 +38,67 @@ pub type Result<T> = result::Result<T, Error>;
 
 mod ioctl {}
 
-/// SPI devices.
-pub enum Device {
-    Spi0 = 0, // Enabled by default
-    Spi1 = 1, // Requires additional configuration
+/// SPI buses.
+///
+/// The Raspberry Pi's GPIO header exposes several SPI buses. SPI0 is available
+/// on all Raspberry Pi models. SPI1 is available on models with a 40-pin
+/// header. SPI2 is only available on the Compute and Compute 3 module.
+///
+/// SPI0 is disabled by default. You can enable it by running
+/// `sudo raspi-config`, or by manually adding `dtparam=spi=on` to
+/// `/boot/config.txt`. The associated pins are listed below.
+///
+/// * MISO: BCM GPIO 9 (physical pin 21)
+/// * MOSI: BCM GPIO 10 (physical pin 19)
+/// * SCLK: BCM GPIO 11 (physical pin 23)
+/// * SS: CE0: BCM GPIO 8 (physical pin 24), CE1: BCM GPIO 7 (physical pin 26)
+///
+/// SPI1 is an auxiliary peripheral that's referred to as mini SPI. According
+/// to the documentation, using higher clock speeds on SPI1 requires additional
+/// CPU time compared to SPI0, caused by shallow FIFOs and no DMA support. SPI1
+/// can be enabled by adding `dtoverlay=spi1-3cs` to `/boot/config.txt`. Replace
+/// `3cs` with either `2cs` or `1cs` if you only require 2 or 1 Slave Select pins.
+/// The associated pins are listed below.
+///
+/// * MISO: BCM GPIO 19 (physical pin 35)
+/// * MOSI: BCM GPIO 20 (physical pin 38)
+/// * SCLK: BCM GPIO 21 (physical pin 40)
+/// * SS: CE0: BCM GPIO 18 (physical pin 12), CE1: BCM GPIO 17 (physical pin 11), CE2: BCM GPIO 16 (physical pin 36)
+///
+/// SPI2 shares the same characteristics as SPI1. It can be enabled by adding
+/// `dtoverlay=spi2-3cs` to `/boot/config.txt`. Replace `3cs` with either `2cs` or
+/// `1cs` if you only require 2 or 1 Slave Select pins. The associated pins are
+/// listed below.
+///
+/// * MISO: BCM GPIO 40
+/// * MOSI: BCM GPIO 41
+/// * SCLK: BCM GPIO 42
+/// * SS: CE0: BCM GPIO 43, CE1: BCM GPIO 44, CE2: BCM GPIO 45
+///
+/// The GPIO pin numbers mentioned above are part of the default configuration. Some of
+/// them can be moved to different pins. Read `/boot/overlays/README` for more information.
+pub enum Bus {
+    Spi0 = 0,
+    Spi1 = 1,
+    Spi2 = 2,
 }
 
-/// Chip Enable pins.
+/// Chip Enable (Slave Select) pins.
+///
+/// Select a Chip Enable pin to signal which device should
+/// interact with the SPI master. Chip Enable is more commonly
+/// known as Slave Select or Chip Select.
+///
+/// Each of the available SPI buses has access to either two
+/// or three Chip Enable pins, as shown below:
+///
+/// * Spi0: Ce0 (BCM GPIO 8), Ce1 (BCM GPIO 7)
+/// * Spi1: Ce0 (BCM GPIO 18, Ce1 (BCM GPIO 17), Ce2 (BCM GPIO 16)
+/// * Spi2: Ce0 (BCM GPIO 43, Ce1 (BCM GPIO 44), Ce2 (BCM GPIO 45)
 pub enum ChipEnable {
-    Ce0 = 0, // SPI0: BCM GPIO 8    SPI1: BCM GPIO 18
-    Ce1 = 1, // SPI0: BCM GPIO 7    SPI1: BCM GPIO 17
-    Ce2 = 2, // SPI0: N/A,          SPI1: BCM GPIO 16
+    Ce0 = 0,
+    Ce1 = 1,
+    Ce2 = 2,
 }
 
 /// SPI modes.
@@ -77,11 +127,11 @@ pub struct Spi {
 }
 
 impl Spi {
-    pub fn new(device: Device, chip_enable: ChipEnable, mode: Mode, speed: u32) -> Result<Spi> {
+    pub fn new(bus: Bus, chip_enable: ChipEnable, mode: Mode, speed: u32) -> Result<Spi> {
         let spidev = OpenOptions::new()
             .read(true)
             .write(true)
-            .open(format!("/dev/spidev{}.{}", device as u8, chip_enable as u8))?;
+            .open(format!("/dev/spidev{}.{}", bus as u8, chip_enable as u8))?;
 
         Ok(Spi { spidev })
     }
