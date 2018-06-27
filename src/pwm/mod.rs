fa87913cc034c6e09e2b7e0a5ab6af647d6e9fa5
Rene van der Meer <rene@golemparts.com>
2018-06-27 20:06:58 +0200

    Update documentation

--- src/pwm/mod.rs
+++ src/pwm/mod.rs
@@ -18,6 +18,39 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+//! Interface for the PWM peripherals.
+//!
+//! ## PWM channels
+//!
+//!
+//!
+//! ## Using PWM without superuser privileges (`sudo`)
+//!
+//! As of kernel version 4.14.34, released on April 16 2018, it's possible to
+//! configure your Raspberry Pi to allow non-root access to PWM. 4.14.34 includes
+//! a [patch] that allows udev to change file permissions when a
+//! PWM channel is exported. This will let any user that's a member of the `gpio`
+//! group configure PWM without having to use `sudo`.
+//!
+//! The udev rules needed to make this work haven't been patched in yet as of
+//! June 2018, but you can easily add them yourself. Make sure you're running
+//! 4.14.34 or later, and append the following snippet to
+//! `/etc/udev/rules.d/99-com.rules`. Reboot the Raspberry Pi afterwards.
+//!
+//! ```
+//! SUBSYSTEM=="pwm*", PROGRAM="/bin/sh -c '\
+//!     chown -R root:gpio /sys/class/pwm && chmod -R 770 /sys/class/pwm;\
+//!     chown -R root:gpio /sys/devices/platform/soc/*.pwm/pwm/pwmchip* &&\
+//!     chmod -R 770 /sys/devices/platform/soc/*.pwm/pwm/pwmchip*\
+//! '"
+//! ```
+//!
+//! ## Troubleshooting
+//!
+//! ### Permission Denied
+//!
+//! [patch]: https://github.com/raspberrypi/linux/issues/1983
+
 pub enum Channel {
     Pwm0,
     Pwm1,
