8c08b65136aaa4fa458ded5cceed87072a6200e7
Rene van der Meer <rene@golemparts.com>
2019-01-05 09:59:08 +0100

    Add LED blinking example

---
+++ examples/gpio_blinkled.rs
@@ -0,0 +1,62 @@
+// Copyright (c) 2017-2019 Rene van der Meer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// gpio_blinkled.rs
+//
+// This example demonstrates how to blink an LED attached to a GPIO pin in a
+// loop.
+//
+// Remember to add a resistor of an appropriate value in series, to prevent
+// exceeding the maximum current rating of the GPIO pin and the LED.
+//
+// Interrupting the loop by pressing Ctrl-C causes the application to exit
+// immediately without resetting the pin's state, so the LED might stay lit.
+// Check out the gpio_blinkled_signals.rs example to learn how to properly
+// handle incoming signals to prevent an abnormal termination.
+
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_LED: u8 = 23;
+
+fn main() {
+    let gpio = Gpio::new().unwrap_or_else(|e| {
+        eprintln!("Error: Can't access GPIO peripheral ({})", e);
+        exit(1);
+    });
+
+    // Retrieve the GPIO pin and configure it as an output.
+    let mut pin = gpio
+        .get(GPIO_LED)
+        .unwrap_or_else(|| {
+            eprintln!("Error: Can't access GPIO pin {}", GPIO_LED);
+            exit(1);
+        })
+        .into_output();
+
+    loop {
+        pin.toggle();
+        sleep(Duration::from_millis(500));
+    }
+}
