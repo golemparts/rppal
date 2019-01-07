29db173e61fef1cfcf9868b46c54a502a9b56055
Rene van der Meer <rene@golemparts.com>
2019-01-07 14:32:58 +0100

    Add PWM example

---
+++ examples/pwm_blinkled.rs
@@ -0,0 +1,61 @@
+// Copyright (c) 2017-2019 Rene van der Meer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// pwm_blinkled.rs
+//
+// Blinks an LED attached to a GPIO pin using PWM.
+//
+// Remember to add a resistor of an appropriate value in series, to prevent
+// exceeding the maximum current rating of the GPIO pin and the LED.
+//
+// Interrupting the process by pressing Ctrl-C causes the application to exit
+// immediately without disabling the PWM channel. Check out the
+// gpio_blinkled_signals.rs example to learn how to properly handle incoming
+// signals to prevent an abnormal termination.
+
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+
+use rppal::pwm::{Channel, Polarity, Pwm};
+
+fn main() {
+    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
+    let pwm =
+        Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true).unwrap_or_else(|e| {
+            eprintln!("Error: Can't access PWM peripheral ({})", e);
+            exit(1);
+        });
+
+    // Sleep for 2 seconds while the LED blinks.
+    sleep(Duration::from_secs(2));
+
+    // Reconfigure the PWM channel for an 8 Hz frequency, 50% duty cycle.
+    pwm.set_frequency(8.0, 0.5).unwrap_or_else(|e| {
+        eprintln!("Error: Invalid frequency or duty cycle ({})", e);
+        exit(1);
+    });
+
+    sleep(Duration::from_secs(3));
+
+    // When the pwm variable goes out of scope at the end of the main()
+    // function, the PWM channel is automatically disabled. You can manually
+    // disable the channel by calling the Pwm::disable() method.
+}
