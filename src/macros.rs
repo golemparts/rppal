dfc47b297e1444b964b2e41f2341bfcfefe717cb
Rene van der Meer <rene@golemparts.com>
2019-11-28 16:29:34 +0100

    Replace deprecated std::mem::uninitialized()
    Fixes #34

--- src/macros.rs
+++ src/macros.rs
@@ -33,12 +33,26 @@ macro_rules! parse_retval {
 // Initialize an array with a type that doesn't implement Copy
 macro_rules! init_array {
     ($val:expr, $len:expr) => {{
-        let mut array: [_; $len] = unsafe { std::mem::uninitialized() };
-        for elem in array.iter_mut() {
-            unsafe {
-                std::ptr::write(elem, $val);
-            }
+        // Based on https://doc.rust-lang.org/stable/std/mem/union.MaybeUninit.html#initializing-an-array-element-by-element
+
+        use std::mem::{self, MaybeUninit};
+
+        // Create an uninitialized array of `MaybeUninit`. The `assume_init` is
+        // safe because the type we are claiming to have initialized here is a
+        // bunch of `MaybeUninit`s, which do not require initialization.
+        let mut data: [MaybeUninit<_>; $len] = unsafe { MaybeUninit::uninit().assume_init() };
+
+        // Dropping a `MaybeUninit` does nothing. Thus using raw pointer
+        // assignment instead of `ptr::write` does not cause the old
+        // uninitialized value to be dropped. Also if there is a panic during
+        // this loop, we have a memory leak, but there is no memory safety
+        // issue.
+        for elem in &mut data[..] {
+            *elem = MaybeUninit::new($val);
         }
-        array
+
+        // Everything is initialized. Transmute the array to the
+        // initialized type.
+        unsafe { mem::transmute::<_, [_; $len]>(data) }
     }};
 }
