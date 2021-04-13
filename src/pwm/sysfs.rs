golemparts/rppal

commit 7ee557f72d9be4efdff11a4aae6585c04cdcb147

Update formatting

src/pwm/sysfs.rs
@@ -30,7 +30,7 @@ use std::result;
 use std::thread;
 use std::time::Duration;
 
-use libc::{c_char, group, passwd};
+use libc::{group, passwd};
 
 use crate::pwm::Polarity;
 
@@ -40,7 +40,7 @@ pub type Result<T> = result::Result<T, io::Error>;
 // Find user ID for specified user
 pub fn user_to_uid(name: &str) -> Option<u32> {
     if let Ok(name_cstr) = CString::new(name) {
-        let buf = &mut [0 as c_char; 4096];
+        let buf = &mut [0_u8; 4096];
         let mut res: *mut passwd = ptr::null_mut();
         let mut pwd = passwd {
             pw_name: ptr::null_mut(),
@@ -73,7 +73,7 @@ pub fn user_to_uid(name: &str) -> Option<u32> {
 // Find group ID for specified group
 pub fn group_to_gid(name: &str) -> Option<u32> {
     if let Ok(name_cstr) = CString::new(name) {
-        let buf = &mut [0 as c_char; 4096];
+        let buf = &mut [0_u8; 4096];
         let mut res: *mut group = ptr::null_mut();
         let mut grp = group {
             gr_name: ptr::null_mut(),

src/gpio/ioctl.rs
@@ -18,6 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+#![allow(clippy::unnecessary_cast)]
 #![allow(dead_code)]
 
 use crate::gpio::{Error, Level, Result, Trigger};
@@ -382,7 +383,7 @@ impl EventData {
                 std::io::ErrorKind::UnexpectedEof,
                 "failed to fill whole buffer",
             )
-                .into())
+            .into())
         } else {
             Ok(event_data)
         }
@@ -431,7 +432,8 @@ pub fn find_gpiochip() -> Result<File> {
 
         let chip_info = ChipInfo::new(gpiochip.as_raw_fd())?;
         if chip_info.label[0..DRIVER_NAME.len()] == DRIVER_NAME[..]
-            || chip_info.label[0..DRIVER_NAME_CM4.len()] == DRIVER_NAME_CM4[..] {
+            || chip_info.label[0..DRIVER_NAME_CM4.len()] == DRIVER_NAME_CM4[..]
+        {
             return Ok(gpiochip);
         }
     }
@@ -452,5 +454,5 @@ fn cbuf_to_cstring(buf: &[u8]) -> CString {
             .unwrap_or_else(|| buf.len());
         &buf[..pos]
     })
-        .unwrap_or_default()
+    .unwrap_or_default()
 }

src/system.rs
@@ -156,10 +156,10 @@ fn parse_proc_cpuinfo() -> Result<Model> {
     let mut revision: String = String::new();
     for line_result in proc_cpuinfo.lines() {
         if let Ok(line) = line_result {
-            if line.starts_with("Hardware\t: ") {
-                hardware = String::from(&line[11..]);
-            } else if line.starts_with("Revision\t: ") {
-                revision = String::from(&line[11..]).to_lowercase();
+            if let Some(line_value) = line.strip_prefix("Hardware\t: ") {
+                hardware = String::from(line_value);
+            } else if let Some(line_value) = line.strip_prefix("Revision\t: ") {
+                revision = String::from(line_value).to_lowercase();
             }
         }
     }

commit a12cd4d0203bdaef362a174e49fb3409d84b6e4a

Cargo fails to build since 7ee557f72d9be4efdff11a4aae6585c04cdcb147

src/pwm/sysfs.rs
@@ -40,7 +40,7 @@ pub type Result<T> = result::Result<T, io::Error>;
 // Find user ID for specified user
 pub fn user_to_uid(name: &str) -> Option<u32> {
     if let Ok(name_cstr) = CString::new(name) {
-        let buf = &mut [0_u8; 4096];
+        let buf = &mut [0_i8; 4096];
         let mut res: *mut passwd = ptr::null_mut();
         let mut pwd = passwd {
             pw_name: ptr::null_mut(),
@@ -73,7 +73,7 @@ pub fn user_to_uid(name: &str) -> Option<u32> {
 // Find group ID for specified group
 pub fn group_to_gid(name: &str) -> Option<u32> {
     if let Ok(name_cstr) = CString::new(name) {
-        let buf = &mut [0_u8; 4096];
+        let buf = &mut [0_i8; 4096];
         let mut res: *mut group = ptr::null_mut();
         let mut grp = group {
             gr_name: ptr::null_mut(),