d169f58ea3622e4e52a88b2b0a3f5def75d08b28
Rene van der Meer <rene@golemparts.com>
2023-12-10 17:17:01 +0100

    Bump version to 0.16.0

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! guaranteed until v1.0.0.
 
 // Used by rustdoc to link other crates to rppal's docs
-#![doc(html_root_url = "https://docs.rs/rppal/0.15.0")]
+#![doc(html_root_url = "https://docs.rs/rppal/0.16.0")]
 
 #[macro_use]
 mod macros;
