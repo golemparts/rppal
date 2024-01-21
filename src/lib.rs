72f828ecc336adf357495102ba5edbbe1a0e8a7b
Rene van der Meer <9930578+golemparts@users.noreply.github.com>
2024-01-21 16:18:59 +0100

    Bump version to 0.17.1

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! guaranteed until v1.0.0.
 
 // Used by rustdoc to link other crates to rppal's docs
-#![doc(html_root_url = "https://docs.rs/rppal/0.17.0")]
+#![doc(html_root_url = "https://docs.rs/rppal/0.17.1")]
 
 #[macro_use]
 mod macros;
